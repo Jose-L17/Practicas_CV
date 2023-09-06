e558067021c1d265fac242733cc95c064c86bd2c
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-06 08:13:22 -0600

    cambio

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    WorkExperienceComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor( private headerService: HeaderService ) { }
+
+  nombre: string = "fake";
+  email:string="fake";
+  cel:string="fake";
+  titulo:any = [];
+  misionVida: string = "fake";
+  ciudad:string="fake";
+  pais:string="fake";
+  redSocial:string="fake";
+  foto: string = "fake";
+
+  ngOnInit(): void {
+    this.headerService.getHeader()
+    .subscribe( (data: any) => {
+      console.log(data.foto);
+      this.nombre = data.name
+      this.email=data.email;
+      this.cel=data.cel;
+      this.titulo=data.titulo;
+      this.misionVida=data.misionVida;
+      this.ciudad=data.ciudad;
+      this.pais=data.pais;
+      this.redSocial=data.redSocial;
+      this.foto=data.foto;
+      alert(data);
+    })
+  }
+
+
+
+
+}
